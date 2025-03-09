775a222cd65433c120340c63828910eaaba2868d
Sumi Nguyen <quynhpn2@uci.edu>
2025-03-09 06:18:25 +0000

    Implemented load_words()

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,4 +1,7 @@
 #include "ladder.h"
+#include <fstream>
+#include <set>
+#include <vector>
 
 
 void error(string word1, string word2, string msg);  // TODO
@@ -7,9 +10,37 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
 
 bool is_adjacent(const string& word1, const string& word2);  // TODO
 
-vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list);  // TODO
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    // TODO
+    queue<string, vector<string>> ladder_queue;
+    ladder_queue.push([begin_word]);
+    set<string> visited;
+    visited.insert(begin_word);
+    while (!ladder_queue.empty()) {
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = ladder.back();
+        for (string word: word_list) {
+            if is_adjacent(last_word, word) {
+                if (word not in visited) {
+                    visited.insert(word);
+                    new_ladder = ladder;
+                    new_ladder.push(word);
+                    if (word == end_word) return new_ladder;
+                    ladder_queue.push(new_ladder);
+                }
+            }
+        }
+    }
+    return "no ladder found";
+}
 
-void load_words(set<string> & word_list, const string& file_name);  // TODO
+void load_words(set<string> & word_list, const string& file_name) {
+    ifstream in(file_name);
+    for (string word; in >> word; )
+        word_list.insert(word);
+    in.close();
+}
 
 void print_word_ladder(const vector<string>& ladder);  // TODO
 
