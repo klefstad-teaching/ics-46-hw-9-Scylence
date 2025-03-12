72e4b33a0e1ca9603bbb8cad2a26b88c73138e60
Sumi Nguyen <quynhpn2@uci.edu>
2025-03-12 05:12:27 +0000

    Implemented edit_distance_within(), is_adjacent(), and generate_word_ladder()

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,4 +1,5 @@
 #include "ladder.h"
+#include <algorithm>
 #include <fstream>
 #include <set>
 #include <vector>
@@ -8,23 +9,44 @@ void error(string word1, string word2, string msg) {
     cout << msg << "\'" << word1 << "\' and \'" << word2 << '\'' << endl;
 }
 
-bool edit_distance_within(const std::string& str1, const std::string& str2, int d);  // TODO
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
+    // Tabulation approach on edit distance dynamic programming problem
+    // Implementation learned from this vid: https://www.youtube.com/watch?v=fJaKO8FbDdo
+    int n = str1.size(), m = str2.size();
+    vector<int> previous(m+1, 0), current(m+1, 0);
+    for (int i = 1; i <= n; ++i) {
+        current[0] = i;
+        for (int j = 1; j <= m; ++j) {
+            if (str1[i-1] == str2[j-1]) current[j] = previous[j-1];
+            else current[j] = 1 + min(previous[j], min(current[j-1], previous[j-1]));
+        }
+        previous = current;
+    }
+    return previous[m] <= d;
+}
 
-bool is_adjacent(const string& word1, const string& word2);  // TODO
+bool is_adjacent(const string& word1, const string& word2) {
+    int length_distance = abs(word1.size() - word2.size());
+    // Length distance > 1 implies word1 needs 2+ operation to connect to word2
+    // so return false immediately;
+    // else call edit_distance_within()
+    return length_distance <= 1 ? edit_distance_within(word1, word2, 1) : false;
+}
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
-    // TODO
-    vector<string> ladder, begin_ladder{begin_word};
+    // Return empty ladder if start word == end word
+    // or else an infinite loop might occur
     if (begin_word == end_word) {
         error(begin_word, end_word, "generate_word_ladder: Cannot perform word ladder search on ");
-        return ladder;
+        return vector<string>();
     }
+    vector<string> ladder, begin_ladder{begin_word};
     queue<vector<string>> ladder_queue;
     ladder_queue.push(begin_ladder);
     set<string> visited;
     visited.insert(begin_word);
     while (!ladder_queue.empty()) {
-        ladder.push_back(ladder_queue.front());
+        ladder = ladder_queue.front();
         ladder_queue.pop();
         string last_word = ladder.back();
         for (string word: word_list) {
@@ -40,7 +62,7 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
         }
     }
     error(begin_word, end_word, "generate_word_ladder: No ladder found for ");
-    return "no ladder found";
+    return vector<string>();  // Returns empty ladder if ladder from start to end word not found
 }
 
 void load_words(set<string> & word_list, const string& file_name) {
@@ -60,20 +82,12 @@ void print_word_ladder(const vector<string>& ladder) {
 void verify_word_ladder() {
 
     set<string> word_list;
-
     load_words(word_list, "words.txt");
 
     my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
-
     my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
-
     my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
-
     my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
-
     my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
-
     my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
-
 }
-
