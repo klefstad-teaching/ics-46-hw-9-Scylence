28924fe7c7529001920b47124d525cfd5dbb4fd9
Sumi Nguyen <quynhpn2@uci.edu>
2025-03-15 07:34:41 +0000

    Updated print_word_ladder() to print space in between the word

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -80,7 +80,7 @@ void load_words(set<string> & word_list, const string& file_name) {
 
 void print_word_ladder(const vector<string>& ladder) {
     for (string word: ladder)
-        cout << word << "->";
+        cout << word << ' ';
 }
 
 #define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
